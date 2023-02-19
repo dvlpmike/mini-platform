97f9af05473548374d40012d4e784dd41fddb1fc
Mike <mgliwicki@gmail.com>
2023-02-19 15:28:20 +0100

    Update vhost-render.sh

--- Jobs/vhost-render.sh
+++ Jobs/vhost-render.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 #
-# Manage virtual hosts
-#
+# Search and render new virtual hosts
+
 # Exit if error
 set -e
 
 # Export variables
 export VHOST_NAME=$1
-export VHOST_NGINX_AVAIL_PATH="/etc/nginx/sites-available/$VHOST_NAME"
-export VHOST_NGINX_ENABL_PATH="/etc/nginx/sites-enabled/$VHOST_NAME"
+export VHOST_PATH_SITES_AVAIL="/etc/nginx/sites-available/$VHOST_NAME"
+export VHOST_PATH_SITES_ENABL="/etc/nginx/sites-enabled/$VHOST_NAME"
+export CONTAINERS_LIST_PATH="/tmp"
+export CONTAINERS_LIST_FILE_NAME="active-containers.txt"
 
-# FUNCTIONS
+# Funtions
+
+#######################################
+# Check active docker containers.
+# Globals:
+#   VHOST_NAME
+#   VHOST_CONFIG_PATH
+# Arguments:
+#   None
+# Outputs:
+#   List names of active docker containers and save it to file.
+#######################################
+function virtual_hosts::check_active_containers {
+    docker ps --format '{{.Names}}' > "$CONTAINERS_LIST_PATH/$CONTAINERS_LIST_FILE_NAME"
+}
 
 #######################################
 # Add new vhost.
 # Globals:
 #   VHOST_NAME
-#   VHOST_NGINX_AVAIL_PATH
-#   VHOST_NGINX_ENABL_PATH
+#   VHOST_CONFIG_PATH
 # Arguments:
-#   VHOST_NAME
+#   None
 # Outputs:
 #   Add vhost to nginx config.
 #######################################
@@ -27,7 +42,52 @@ function virtual_hosts::add_vhost {
     local CONTAINER_LOCAL_HOST="http://localhost"
     local CONTAINER_LOCAL_PORT=$(docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}} {{end}}' $VHOST_NAME)
     local CONTAINER_LOCAL_URL="$CONTAINER_LOCAL_HOST:$CONTAINER_LOCAL_PORT"
-    cat > $VHOST_NGINX_AVAIL_PATH << EOF
+    echo "server {
+        listen 80;
+        listen [::]:80;
+        server_name $VHOST_NAME www.$VHOST_NAME;
+        location / {
+                proxy_pass $CONTAINER_LOCAL_URL;
+        }
+    }" > $VHOST_PATH_SITES_AVAIL
+    ln -s $VHOST_PATH_SITES_AVAIL /etc/nginx/sites-enabled/
+    systemctl reload nginx
+}
+
+#######################################
+# Delete vhost.
+# Globals:
+#   VHOST_NAME
+# Arguments:
+#   None
+# Outputs:
+#   Delete vhost from nginx config.
+#######################################
+function virtual_hosts::delete_vhost {
+    rm -f $VHOST_PATH_SITES_AVAIL
+    rm -f $VHOST_PATH_SITES_ENABL
+}
+
+#######################################
+# Check vhosts.
+# Globals:
+#   CONTAINERS_LIST_PATH
+#   CONTAINERS_LIST_FILE_NAME
+# Arguments:
+#   None
+# Outputs:
+#   Check if vhost exists.
+#######################################
+function virtual_hosts::check_vhosts {
+    local LIN=$(cat $CONTAINERS_LIST_PATH/$CONTAINERS_LIST_FILE_NAME)
+    local VHOST_CONFIG_PATH2="/etc/nginx/sites-available"
+    for i in $LIN
+    do
+        if [ -f "$VHOST_CONFIG_PATH2/$i" ]; then
+            echo "$i jest. nie dodawaj"
+        else
+            echo "$i brak. dodaj"
+            cat > /etc/nginx/sites-available/$VHOST_NAME << EOF
         server {
             listen 80;
             listen [::]:80;
@@ -37,53 +97,97 @@ function virtual_hosts::add_vhost {
             }
         }
 EOF
-    ln -s $VHOST_NGINX_AVAIL_PATH $VHOST_NGINX_ENABL_PATH
-    systemctl reload nginx
+        fi
+    done
 }
 
 #######################################
-# Delete vhost.
+# Delete certbot certificate.
 # Globals:
-#   VHOST_NGINX_AVAIL_PATH
-#   VHOST_NGINX_ENABL_PATH
+#   VHOST_NAME
+# Arguments:
+#   VHOST_NAME
 # Outputs:
-#   Delete vhost from nginx config.
+#   The function takes one argument, check if the certificate exists and remove it.
 #######################################
-function virtual_hosts::delete_vhost {
-    rm $VHOST_NGINX_AVAIL_PATH
-    rm $VHOST_NGINX_ENABL_PATH
+function virtual_hosts::delete_certificate {
+  if [[ -z "$VHOST_NAME" ]]; then
+    echo "Please provide the domain name as an argument."
+    return 1
+  fi
+
+  if ! sudo certbot certificates | grep -q "$1"; then
+    echo "Certificate for $VHOST_NAME does not exist."
+    return 1
+  fi
+
+  sudo certbot delete --non-interactive --cert-name "$VHOST_NAME"
+  sudo service nginx reload
+  echo "Certificate for $VHOST_NAME has been deleted."
+  sleep 20
+}
+
+#######################################
+# Create certbot certificate.
+# Globals:
+#   VHOST_NAME
+# Arguments:
+#   VHOST_NAME
+# Outputs:
+#   The function takes one argument, check if the certificate exists and create it.
+#######################################
+function virtual_hosts::create_certificate {
+  if [[ -z "$VHOST_NAME" ]]; then
+    echo "Please provide the domain name as an argument."
+    return 1
+  fi
+  if sudo certbot certificates | grep -q "$1"; then
+    echo "Certificate for $VHOST_NAME already exists."
+    return 1
+  fi
+
+  sudo certbot --nginx --non-interactive -d "$VHOST_NAME"
+  sudo service nginx reload
+  echo "Certificate for $VHOST_NAME has been created."
 }
 
 #######################################
 # Check vhost.
 # Globals:
-#   VHOST_NAME
-#   VHOST_NGINX_AVAIL_PATH
-#   VHOST_NGINX_ENABL_PATH
+#   CONTAINERS_LIST_PATH
+#   CONTAINERS_LIST_FILE_NAME
 # Arguments:
-#   VHOST_NAME
+#   None
 # Outputs:
 #   Check if vhost exists.
 #######################################
 function virtual_hosts::check_vhost {
-    if [ -f "$VHOST_NGINX_AVAIL_PATH" ] && [ -f "$VHOST_NGINX_ENABL_PATH" ] ; then
-        echo "$VHOST_NAME exists. Delete and add as a new"
-        virtual_hosts::delete_vhost
-        virtual_hosts::add_vhost
-    else
-        echo "$VHOST_NAME not exists. Add"
-        virtual_hosts::add_vhost
-    fi
+  if [ -f $VHOST_PATH_SITES_AVAIL ] || [ -L $VHOST_PATH_SITES_ENABL ] ; then
+    echo "$VHOST_NAME already exists. It will be remove and add as new"
+    virtual_hosts::delete_vhost
+    echo "$VHOST_PATH_SITES_AVAIL and $VHOST_PATH_SITES_ENABL removed"
+    virtual_hosts::add_vhost
+  else
+    echo "$VHOST_NAME brak. dodaj"
+    virtual_hosts::add_vhost
+  fi
 }
 
 #######################################
 # Main function.
+# Globals:
+#
+#
+# Arguments:
 #
 # Outputs:
-#   Render vhost.
+#
 #######################################
 function virtual_hosts::main {
+    #virtual_hosts::check_active_containers
     virtual_hosts::check_vhost
+    virtual_hosts::delete_certificate
+    virtual_hosts::create_certificate
 }
 
 virtual_hosts::main
